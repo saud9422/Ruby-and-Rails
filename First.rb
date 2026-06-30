6c57807704312a6a4ceb6cf0b10795b571e0abf4
Saud Khan <skmaster313@gmail.com>
2026-06-30 19:04:45 +0530

    new file added

---
+++ First.rb
@@ -0,0 +1,15 @@
+
+module FirstModule
+    def greet
+        puts "Greetings from FirstModule!"
+    end
+end
+
+class First
+    VERSION = "1.0.0"
+    puts "Hello, World!"
+
+    include FirstModule
+end
+
+puts First::VERSION
